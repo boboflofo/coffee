b7c5651ac8b1eac861259c9f52de466317fc7f22
Jonathan Song <jonats10@uci.edu>
2023-12-08 10:46:12 -0800

    connect files and initialize control class

---
+++ src/components/CoffeeControl.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import CoffeeList from "./CoffeeList"
+import CoffeeDetail from "./CoffeeDetail"
+import CoffeeForm from "./CoffeeForm"
+
+
+export default class CoffeeControl extends React.Component{
+
+  constructor() {
+    
+  }
+}
+
